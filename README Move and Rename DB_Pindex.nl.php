36c9effac19ea3cc712bd2b4f087eb589000a8a6
fabian1983 <fabian@alphen.be>
2025-05-05 00:35:05 +0200

    db

--- README Move and Rename DB_Pindex.nl.php
+++ README Move and Rename DB_Pindex.nl.php
@@ -4,15 +4,17 @@
 // Example place this file in /.../www/ == Not vissble for public dir, so this can not entered by visitors, only by your http server
 
 //SQL LOGIN
-define('DB_SERVER', 'localhost:3307');
+define('DB_SERVER', 'localhost'); //localhost:3307
 define('DB_USERNAME', 'username');
 define('DB_PASSWORD', 'password');
 define('DB_NAME', 'database');
  
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
+// Create connection 
+$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
 // Check connection
-if($link === false){
-    echo "died on making connection"; 
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+if ($conn->connect_error) {
+	echo "died on making connection"; 
+	die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+else { echo "DB Connected successfully"; } 
